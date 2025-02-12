7dd6ad852cec80797a26ec9c64de0551ee21594b
butterflypaw <nithya.etoori@gmail.com>
2025-02-12 23:00:22 +0530

    Day 5

---
+++ Tapadia sir/Day5/Q2.java
@@ -0,0 +1,30 @@
+// You are given a singly linked list containing N nodes. 
+// Your task is to find the middle element of the linked list.
+
+// Input Format:
+// -------------
+// Line 1: An integer N, representing the number of nodes in the linked list.
+// Line 2: N space-separated integers representing the elements of the linked list.
+
+// Output Format:
+// --------------
+// Line-1: Print a single integer, the middle element of the linked list.
+
+// Sample Input-1:
+// ---------------
+// 5
+// 1 2 3 4 5
+
+// Sample Output-1:
+// ----------------
+// 3
+
+
+// Sample Input-2:
+// ---------------
+// 6
+// 1 2 3 4 5 6
+
+// Sample Output-2:
+// ----------------
+// 4
---
+++ Tapadia sir/Day5/Q3.java
@@ -0,0 +1,95 @@
+// [6:09 pm, 12/2/2025] ~Nithya: You are given a 1-indexed array of integers, numbers, sorted in non-decreasing order. 
+// Your task is to find two distinct numbers in the array such that their sum equals a given target.
+
+// Return their 1-based indices as an array [index1, index2], where 1 ≤ index1 < index2 ≤ numbers.length.
+
+// Constraints:
+// ------------
+// -> There is exactly one valid solution.
+// -> You cannot use the same element twice.
+// -> Your solution must use only constant extra space.
+
+// Input Format:
+// -------------
+// Line-1: An integer N, representing the size of the array.
+// Line-2: N space-separated integers, representing the array elements.
+// Line-3: An integer X, representing the target sum.
+
+// Output Format:
+// --------------
+// Line-1: Print indices as an array if there exists a pair whose sum is equal to X
+
+// Sample Input-1:
+// ---------------
+// 4
+// 2 7 11 15
+// 9
+
+// Sample Output-1:
+// ----------------
+// [1, 2]
+
+// Explanation:
+// -------------
+// The sum of 2 and 7 is 9.
+// Their 1-based indices are 1 and 2.
+
+// Sample Input-2:
+// ---------------
+// 3
+// 2 3 4
+// 6
+
+// Sample Output-2:
+// ----------------
+// [1, 3]
+
+// Explanation:
+// ------------
+// The sum of 2 and 4 is 6.
+// Their 1-based indices are 1 and 3.
+
+// Sample Input-3:
+// ---------------
+// 2
+// -1 0
+// -1
+
+// Sample Output-3:
+// ----------------
+// [1, 2]
+
+// Explanation:
+// ------------
+// The sum of -1 and 0 is -1.
+// Their 1-based indices are 1 and 2.
+// [6:09 pm, 12/2/2025] ~Nithya: You are given a singly linked list containing N nodes. 
+// Your task is to find the middle element of the linked list.
+
+// Input Format:
+// -------------
+// Line 1: An integer N, representing the number of nodes in the linked list.
+// Line 2: N space-separated integers representing the elements of the linked list.
+
+// Output Format:
+// --------------
+// Line-1: Print a single integer, the middle element of the linked list.
+
+// Sample Input-1:
+// ---------------
+// 5
+// 1 2 3 4 5
+
+// Sample Output-1:
+// ----------------
+// 3
+
+
+// Sample Input-2:
+// ---------------
+// 6
+// 1 2 3 4 5 6
+
+// Sample Output-2:
+// ----------------
+// 4
